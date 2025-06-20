a0473d8aabad76e5e0598ad4081fb7b3236e72b2
Alex@ander <vasquez.fredy@eax.edu.sv>
2025-06-20 16:58:28 -0600

    feat: Se agregan funciones StarGame, CheckMatch, flipCard, createCard  inyectadas al DOM

--- src/main.js
+++ src/main.js
@@ -35,3 +35,105 @@ const symbols = [
   'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
   'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
 ];
+
+
+// Variables globales
+let cards = [];
+let flippedCards = [];
+let lockBoard = false;
+let score = 0;
+
+// Elementos capturados del DOM
+const gameBoard = document.getElementById('gameBoard');
+const scoreSpan = document.querySelector('#score span');
+const startBtn = document.getElementById('startBtn');
+
+
+// Eventos de los botones
+startBtn.addEventListener('click', startGame);
+
+
+
+// Funcion que Crea las tarjetas y las mezcla
+function createCards() {
+  const fullSet = [...symbols, ...symbols];
+  const shuffled = fullSet.sort(() => 0.5 - Math.random());
+
+  gameBoard.innerHTML = '';
+  cards = [];
+
+  shuffled.forEach(symbol => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.dataset.symbol = symbol;
+
+    card.innerHTML = `
+      <div class="card-inner">
+      <div class="card-front">
+        <img src="${symbol}" alt="logo" />
+      </div>
+        <div class="card-back">❔</div>
+      </div>
+    `;
+
+    card.addEventListener('click', () => flipCard(card));
+    gameBoard.appendChild(card);
+    cards.push(card);
+  });
+}
+
+// Funcion que voltea las tarjetas y verifica si hay coincidencias
+function flipCard(card) {
+  if (lockBoard || card.classList.contains('matched') || card.classList.contains('flipped')) return;
+
+  card.classList.add('flipped');
+  flippedCards.push(card);
+
+  if (flippedCards.length === 2) {
+    lockBoard = true;
+    checkMatch();
+  }
+}
+
+
+// Funcion que verifica si las tarjetas volteadas son iguales
+function checkMatch() {
+  const [card1, card2] = flippedCards;
+  const isMatch = card1.dataset.symbol === card2.dataset.symbol;
+
+  setTimeout(() => {
+    if (isMatch) {
+      card1.classList.add('matched');
+      card2.classList.add('matched');
+      score++;
+      scoreSpan.textContent = score;
+    } else {
+      card1.classList.remove('flipped');
+      card2.classList.remove('flipped');
+    }
+    flippedCards = [];
+    lockBoard = false;
+  }, 2000);
+}
+
+// Funcion que inicia el juego, crea las tarjetas y muestra las cartas por un tiempo breve
+function startGame() {
+  createCards();
+  score = 0;
+  
+  flippedCards = [];
+  lockBoard = true;
+
+  // Mostramos los logos por un breve tiempo
+  setTimeout(() => {
+    cards.forEach(card => card.classList.add('flipped'));
+
+    // Ocultamos después de 1 segundo (quitamos flipped)
+    setTimeout(() => {
+      cards.forEach(card => card.classList.remove('flipped'));
+      lockBoard = false;
+    }, 1000);
+
+  }, 50); // Esperamos a que el DOM se actualice
+}
+
